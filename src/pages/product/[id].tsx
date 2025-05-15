4a46c723b849a23f39b7cfd9f28c71fa9a9f2e5f
juniorBM <juniorbev.mendonca@gmail.com>
2025-05-15 20:51:40 -0300

    Terminado projeto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,109 @@
+import { stripe } from "@/lib/stripe";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/styles/pages/product";
+import axios from "axios";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Image from "next/image";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import Stripe from "stripe";
 
-export default function Product() {
-  const { query } = useRouter();
-  return <h1>Product : {JSON.stringify(query)}</h1>;
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+    defaultPriceId: string;
+  };
 }
+
+export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter();
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] =
+    useState(false);
+
+  if (isFallback) {
+    return <p>Loading...</p>;
+  }
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true);
+      const response = await axios.post("/api/checkout", {
+        priceId: product.defaultPriceId,
+      });
+
+      const { checkoutUrl } = response.data;
+      window.location.href = checkoutUrl;
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+      setIsCreatingCheckoutSession(false);
+      alert(err);
+    }
+  }
+
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
+
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: { id: "prod_SIzRH59RLyN9hN" },
+      },
+    ],
+    fallback: true,
+  };
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params?.id ?? "";
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as Stripe.Price;
+  const valuePrice = price.unit_amount ?? 0;
+  const formattedPrice = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(valuePrice / 100) as string;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: formattedPrice,
+        description: product.description,
+        defaultPriceId: price.id,
+      },
+    },
+    revalidate: 60 * 60 * 1,
+  };
+};
